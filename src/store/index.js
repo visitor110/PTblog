e4fbe03ae71b55daa06cda149c3f15628cae634c
visitor110 <1187038691@qq.com>
2020-03-03 16:29:11 +0800

    博客展示0.91 vuex自动存到localStorage,刷新自动获取

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import permission from './model/permission'
 import userInfo from './model/userInfo'
+import createVuexAlong from 'vuex-along'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
-    permission,
-    userInfo
-  }
+    permission: permission,
+    userInfo: userInfo,
+  },
+  plugins: [
+    createVuexAlong({
+      name: 'PTblog-vuex-along',
+      local: {
+        list: ['permission', 'userInfo'],
+      }
+    })]
 })
